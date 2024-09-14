998a4e5a178b6cf8c3635da9d54d453a7a19f216
ArunRathaur1 <arunrathaur92.6@gmail.com>
2024-09-15 04:44:32 +0530

    checking again

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -94,8 +94,8 @@ app.post("/fetchuser", fetchuser, async (req, res) => {
 app.post('/submit', async (req, res) => {
   try {
     const { player, team, opponent, status, model_name } = req.body;
-    const command = `python python/main.py ${model_name} "${player}" "${team}" "${opponent}" ${status}`;
-
+    //const command = `python python/main.py ${model_name} "${player}" "${team}" "${opponent}" ${status}`;
+    const command = 'ls';
     console.log(`Executing command: ${command}`);
 
     exec(command, (error, stdout, stderr) => {
@@ -130,8 +130,8 @@ app.post('/submit', async (req, res) => {
 app.post('/getreport', async (req, res) => {
   try {
     const { player } = req.body;
-    // const command = `python ../python/reportGen.py "${player}"`;
-    const command = 'ls';
+    const command = `python ../python/reportGen.py "${player}"`;
+    
 
     console.log(`Executing command: ${command}`);
 
